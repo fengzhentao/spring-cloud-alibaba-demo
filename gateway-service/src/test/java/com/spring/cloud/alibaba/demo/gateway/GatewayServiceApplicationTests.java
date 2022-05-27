aec422033b638c35e64f4fc6f5e40b342838c278
fengzt <13651149970@163.com>
2022-05-27 10:22:29 +0800

    init

---
+++ gateway-service/src/test/java/com/spring/cloud/alibaba/demo/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.alibaba.demo.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
